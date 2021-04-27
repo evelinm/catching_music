238387027e425e582cd0db7e72799d67eafd7e8c
evelinm <mandamurillo910@gmail.com>
2021-04-27 15:00:26 -0700

    adding components/reducers/routers

---
+++ catching_music/src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Header = () => {
+return (
+<div>
+<h1 className="main-heading">Spotify Music Search</h1>;
+</div>
+);
+}
+export default Header;
+
+
---
+++ catching_music/src/Components/RedirectPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const RedirectPage = () => {
+ return ( 
+ <div>Redirect Page</div>
+ );
+};
+export default RedirectPage
